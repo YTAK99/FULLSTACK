fdbce7bfe960ce9bc8c2be25aeef1c049809ca0f
YTAK99 <ytzzang99@gmail.com>
2026-07-01 18:45:52 +0900

    0701

--- mall/src/router/root.js
+++ mall/src/router/root.js
@@ -5,15 +5,27 @@ import {createBrowserRouter} from "react-router-dom";
 const Loading = <div>Loading...</div>
 const Main = lazy(() => import("../pages/MainPage"))
 const About = lazy(() => import("../pages/AboutPage"))
+const TodoIndex = lazy(() => import("../pages/todo/IndexPage"))
+const TodoList = lazy(()=>import("../pages/todo/ListPage"))
 
 const root = createBrowserRouter([
     {
         path: "",
-        element: <Suspense fallback={Loading}><Main /></Suspense>
+        element: <Suspense fallback={Loading}><Main/></Suspense>
     },
     {
         path: "about",
-        element: <Suspense fallback={Loading}><About /></Suspense>
+        element: <Suspense fallback={Loading}><About/></Suspense>
+    },
+    {
+        path: "todo",
+        element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
+        children: [
+            {
+                path: "list",
+                element: <Suspense fallback={Loading}><TodoList/></Suspense>
+            }
+        ]
     }
 ])
 
