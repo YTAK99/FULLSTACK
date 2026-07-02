88ffa476ddcec90f515311aae267c20242a8345f
YTAK99 <ytzzang99@gmail.com>
2026-07-02 15:18:50 +0900

    0702

---
+++ mallapi/src/main/java/org/zerock/mallapi/domain/Todo.java
@@ -0,0 +1,28 @@
+package org.zerock.mallapi.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tbl-todo")
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long tno;
+
+    private String title;
+
+    private String writer;
+
+    private boolean complete;
+
+    private LocalDate dueDate;
+}
---
+++ mallapi/src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
@@ -0,0 +1,36 @@
+package org.zerock.mallapi.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.mallapi.domain.Todo;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+@Log4j2
+public class TodoRepositoryTests {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Test
+    public void test1() {
+        log.info("-------------------------");
+        log.info(todoRepository);
+    }
+
+    @Test
+    public void testInsert() {
+        for (int i=1; i<=100; i++) {
+            Todo todo = Todo.builder()
+                    .title("Title..." + i)
+                    .dueDate(LocalDate.of(2026,07,01))
+                    .writer("user00")
+                    .build();
+
+            todoRepository.save(todo);
+        }
+    }
+}
