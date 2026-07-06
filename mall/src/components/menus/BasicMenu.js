2ba60fb93966a56178b937934286355febb24f96
YTAK99 <ytzzang99@gmail.com>
2026-07-06 18:48:06 +0900

    0706

--- mall/src/components/menus/BasicMenu.js
+++ mall/src/components/menus/BasicMenu.js
@@ -1,8 +1,9 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom"
 
 const BasicMenu = () => {
     return (
         <nav id='navbar' className="flex bg-blue-300">
+
             <div className="w-4/5 bg-gray-500">
                 <ul className="flex p-4 text-white font-bold">
                     <li className="pr-6 text-2xl">
@@ -18,9 +19,11 @@ const BasicMenu = () => {
             </div>
 
             <div className="w-1/5 flex justify-end bg-orange-300 p-4 font-medium">
-                <div className="text-white text-sm m-1 rounded">Login</div>
+                <div className="text-white text-sm m-1 rounded">
+                    Login
+                </div>
             </div>
         </nav>
-    );
+    )
 }
 export default BasicMenu;
--- mall/src/router/root.js
+++ mall/src/router/root.js
@@ -10,19 +10,31 @@ const TodoIndex = lazy(() => import("../pages/todo/IndexPage"))
 const TodoList = lazy(()=>import("../pages/todo/ListPage"))
 
 const root = createBrowserRouter([
-    {
-        path: "",
-        element: <Suspense fallback={Loading}><Main/></Suspense>
-    },
-    {
-        path: "about",
-        element: <Suspense fallback={Loading}><About/></Suspense>
-    },
-    {
-        path: "todo",
-        element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
-        children: todoRouter()
-    }
-])
+  {
+    path: "",
+    element: (
+      <Suspense fallback={Loading}>
+        <Main />
+      </Suspense>
+    ),
+  },
+  {
+    path: "about",
+    element: (
+      <Suspense fallback={Loading}>
+        <About />
+      </Suspense>
+    ),
+  },
+  {
+    path: "todo",
+    element: (
+      <Suspense fallback={Loading}>
+        <TodoIndex />
+      </Suspense>
+    ),
+    children: todoRouter(),
+  },
+]);
 
 export default root;
